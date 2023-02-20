5d4c9af4b8765769c3567df4991c96a70b4551c7
catemohi <catemohi@gmail.com>
2023-02-20 23:26:27 +0300

    ...

--- beward_cgi/intercomdu.py
+++ beward_cgi/intercomdu.py
@@ -14,7 +14,6 @@ class IntercomduModule(BewardIntercomModule):
         password=None,
         cgi="cgi-bin/intercomdu_cgi",
     ):
-        raise NotImplementedError
         super(IntercomduModule, self).__init__(
             client,
             ip,
@@ -22,3 +21,64 @@ class IntercomduModule(BewardIntercomModule):
             password,
             cgi,
         )
+
+    def load_params(self):
+        """Метод получения параметров установленных на панели."""
+
+        table_index = self._get_table_index()
+        dozens = self._get_table_dozens()
+        units = self._get_table_units()
+        print(table_index, dozens, units)
+
+    def _get_table_index(self):
+        """Получить индекс таблиц"""
+        code = 200
+        index = 0
+        while code == 200:
+            response = self.client.query(
+                setting=self.cgi,
+                params={
+                    "action": "get",
+                    "Index": str(index),
+                },
+            )
+            response = self.client.parse_response(response)
+            code = response.get("code")
+            index += 1
+        return index
+
+    def _get_table_dozens(self):
+        """Получить количество десятков таблицы"""
+        code = 200
+        dozens = 0
+        while code == 200:
+            response = self.client.query(
+                setting=self.cgi,
+                params={
+                    "action": "get",
+                    "Index": "0",
+                    "Dozens": str(dozens),
+                },
+            )
+            response = self.client.parse_response(response)
+            code = response.get("code")
+            dozens += 1
+        return dozens
+
+    def _get_table_units(self):
+        """Получить количество едениц в десяке"""
+        code = 200
+        units = 0
+        while code == 200:
+            response = self.client.query(
+                setting=self.cgi,
+                params={
+                    "action": "get",
+                    "Index": "0",
+                    "Dozens": str(units),
+                },
+            )
+            response = self.client.parse_response(response)
+            code = response.get("code")
+            units += 1
+        return units
