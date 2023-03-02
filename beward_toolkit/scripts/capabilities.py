30efbd81e9640b526285199d044d7e5bc927ab8e
catemohi <catemohi@gmail.com>
2023-03-03 01:22:12 +0300

    ...

--- beward_toolkit/scripts/capabilities.py
+++ beward_toolkit/scripts/capabilities.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from sys import path
 
 if str(Path(__file__).resolve().parent.parent) not in path:
+    path.append(str(Path(__file__).resolve().parent.parent))
+if str(Path(__file__).resolve().parent.parent.parent) not in path:
     path.append(str(Path(__file__).resolve().parent.parent.parent))
 
 from beward_cgi.user_capabilities import UserCapabilitiesModule
--- beward_toolkit/scripts/credentials.py
+++ beward_toolkit/scripts/credentials.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from sys import path
 
 if str(Path(__file__).resolve().parent.parent) not in path:
+    path.append(str(Path(__file__).resolve().parent.parent))
+if str(Path(__file__).resolve().parent.parent.parent) not in path:
     path.append(str(Path(__file__).resolve().parent.parent.parent))
 
 from config.settings import PASSWORDS, PASSWORDS_BASE
