dce54d3778ad457e6ede7d8aa323775a3daf256e
catemohi <catemohi@gmail.com>
2023-03-03 01:25:10 +0300

    ...

--- beward_cgi/user_capabilities.py
+++ beward_cgi/user_capabilities.py
@@ -18,7 +18,7 @@ class UserCapabilitiesModule(BewardIntercomModule):
 
     def load_params(self):
         """Метод получения параметров установленных на панели."""
-
+        print(self.client)
         response = self.client.query(setting=self.cgi, params={"action": "get"})
 
         response = self.client.parse_response(response)
