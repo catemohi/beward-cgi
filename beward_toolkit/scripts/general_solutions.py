73875259f6ed630cb644ef882cfa47948b9883d2
catemohi <catemohi@gmail.com>
2023-03-03 14:55:10 +0300

    ..

--- beward_toolkit/scripts/general_solutions.py
+++ beward_toolkit/scripts/general_solutions.py
@@ -10,6 +10,9 @@ def run_command_to_seqens(command, seqens, iteration_kwargs_names=()):
         seqens (tuple): последовательность аргументов
         iteration_kwargs_names (tuple, optional): . Defaults to ().
     """
+    print(iteration_kwargs_names)
+    print(seqens)
     for args in seqens:
         kwargs = dict(zip(iteration_kwargs_names, args))
+        print(kwargs)
         command(**kwargs)
